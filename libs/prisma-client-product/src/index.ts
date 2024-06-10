2b19d3905bb9f75c07bf3996b39f8cf0b93f9a91
Vitaliy Bobrov <v.bobrov.dvpb@gmail.com>
2024-06-10 12:18:58 +0300

    add user, product prisma-client libs

---
+++ libs/prisma-client-product/src/index.ts
@@ -0,0 +1,4 @@
+export { Category, Prisma, Product } from '@prisma/client/product';
+
+export * from './lib/prisma-client-product.module';
+export * from './lib/prisma.service';
---
+++ libs/prisma-client-user/src/index.ts
@@ -0,0 +1,4 @@
+export { Prisma, User } from '@prisma/client/user';
+
+export * from './lib/prisma-client-user.module';
+export * from './lib/prisma.service';
